c7504cac39f652d56c165c458584311d70110289
gentlementlegen <fernand.veyrier@epitech.eu>
2025-07-22 16:36:59 +0200

    chore: adjust type imports to use `type` modifier and update embedding import paths

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -1,3 +1,3 @@
-import { components } from "@octokit/openapi-types/types.d.ts";
+import type { components } from "@octokit/openapi-types/types.d.ts";
 
 export type Comment = components["schemas"]["issue-comment"];
