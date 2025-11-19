d466ed483720a0f07f951967ccf0def6db12d562
gentlementlegen <fernand.veyrier@epitech.eu>
2025-11-19 17:44:12 +0900

    chore(WIP): swagger ui

--- src/worker.ts
+++ src/worker.ts
@@ -1,8 +1,12 @@
+import { swaggerUI } from "@hono/swagger-ui";
 import { createPlugin } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
 import { LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import type { ExecutionContext } from "hono";
+import { describeRoute, openAPIRouteHandler, resolver, validator } from "hono-openapi";
+import * as v from "valibot";
 import manifest from "../manifest.json" with { type: "json" };
+import pkg from "../package.json" with { type: "json" };
 import { createAdapters } from "./adapters/index";
 import { runPlugin } from "./plugin";
 import { Command } from "./types/command";
@@ -10,9 +14,15 @@ import { SupportedEvents } from "./types/context";
 import { Env, envSchema } from "./types/env";
 import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
 
+const querySchema = v.object({
+  issueUrl: v.union([v.array(v.string()), v.string()]),
+});
+
+const responseSchema = v.array(v.object({ userId: v.string(), score: v.number() }));
+
 export default {
   async fetch(request: Request, env: Env, executionCtx?: ExecutionContext) {
-    return createPlugin<PluginSettings, Env, Command, SupportedEvents>(
+    const honoApp = createPlugin<PluginSettings, Env, Command, SupportedEvents>(
       (context) => {
         return runPlugin({
           ...context,
@@ -28,6 +38,42 @@ export default {
         kernelPublicKey: env.KERNEL_PUBLIC_KEY,
         bypassSignatureVerification: process.env.NODE_ENV === "local",
       }
-    ).fetch(request, env, executionCtx);
+    );
+
+    honoApp.get(
+      "/recommendations",
+      describeRoute({
+        description: "Get recommended users for a given issue url",
+        responses: {
+          200: {
+            description: "Successful response",
+            content: {
+              "application/json": { schema: resolver(responseSchema) },
+            },
+          },
+        },
+      }),
+      validator("query", querySchema),
+      () => new Response("OK")
+    );
+    honoApp.get(
+      "/openapi",
+      openAPIRouteHandler(honoApp, {
+        documentation: {
+          info: {
+            title: pkg.name,
+            version: pkg.version,
+            description: pkg.description,
+          },
+          servers: [
+            { url: "http://localhost:4004", description: "Local Server" },
+            { url: manifest.homepage_url, description: "Production Server" },
+          ],
+        },
+      })
+    );
+    honoApp.get("/ui", swaggerUI({ url: "/openapi" }));
+
+    return honoApp.fetch(request, env, executionCtx);
   },
 };
