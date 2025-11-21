cc437e1d95abf3df889660431631db82e721194e
gentlementlegen <fernand.veyrier@epitech.eu>
2025-11-21 17:15:43 +0900

    chore: update `responseSchema` to handle detailed match results and similar issues in `worker.ts`

--- src/worker.ts
+++ src/worker.ts
@@ -27,7 +27,29 @@ const querySchema = v.object({
   issueUrls: v.union([v.array(urlSchema), urlSchema]),
 });
 
-const responseSchema = v.array(v.unknown());
+const responseSchema = v.record(
+  v.string(),
+  v.union([
+    v.object({
+      matchResultArray: v.map(v.string(), v.array(v.string())),
+      similarIssues: v.array(
+        v.object({
+          id: v.string(),
+          issue_id: v.string(),
+          similarity: v.number(),
+        })
+      ),
+      sortedContributors: v.array(
+        v.object({
+          login: v.string(),
+          matches: v.array(v.string()),
+          maxSimilarity: v.number(),
+        })
+      ),
+    }),
+    v.null(),
+  ])
+);
 
 export default {
   async fetch(request: Request, environment: Env, executionCtx?: ExecutionContext) {
